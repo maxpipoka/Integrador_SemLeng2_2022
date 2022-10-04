0b7c03ba8ae0ec6de3fa6105922e39aa514c86f8
maxpipoka <martincardozo@gmail.com>
2022-10-04 20:10:02 -0300

    Modicado el App.java con ejemplo de modificacion.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!. Este es un archivo modificado en local");
     }
 }

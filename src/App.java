72b0b75817b6629da779d64b9175494fd57a7fe7
maxpipoka <martincardozo@gmail.com>
2022-11-05 14:47:54 -0300

    Realizada la configuracion de la DB, y escrito el codigo en App.java para un primer guardado de ejemplo

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import models.basic.Instituto;
+
+
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -12,14 +15,26 @@ public class App {
         // Luego se llama al método 'createEntityManagerFactory' de la clase Persistence pasandole
         // el nombre de la PERSISTENCE_UNIT del persistence.xml que se desea manejar con ese EntityManagerFactory
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FacultadPersistance");
+        EntityManagerFactory emf_facultad = Persistence.createEntityManagerFactory("FacultadPersistance");
 
         // Ahora usando la Factory se crea un EntityManager
         // Especificamos el tipo de objeto "EntityManager" y seguido le asignamos nombre "em"
         // Luego llamamos al método 'createEntityManager' de la clase 'emf' que creamos en la linea 15
 
-        EntityManager em = emf.createEntityManager();
+        EntityManager em_entidadesFacultad = emf_facultad.createEntityManager();
 
+        // Iniciamos una transaccion
+        em_entidadesFacultad.getTransaction().begin();
 
+        //creamos un Instituto inicial
+        Instituto inst1 = new Instituto(1, "Primer instituto");
+
+        // Preparamos el guardado del insituto creado en "inst1"
+        em_entidadesFacultad.persist(inst1);
+
+        // Confirmamos la transaccion
+        em_entidadesFacultad.getTransaction().commit();
+
+        System.out.println("Termino sin errores");
     }
 }

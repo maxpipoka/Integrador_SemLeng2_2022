9e91ec394b8abbbd9ba3e07bf443bd7673013245
maxpipoka <martincardozo@gmail.com>
2022-12-12 17:34:11 -0300

    MainVista and MainVistaController maked. FacultadVista deleted

---
+++ src/controller/fx/MainVistaController.java
@@ -0,0 +1,27 @@
+package controller.fx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class MainVistaController {
+
+    @FXML
+    private Button btnInstitutos;
+
+    @FXML
+    private Button btnCarreras;
+
+    @FXML
+    private Button btnAsignaturas;
+
+    @FXML
+    private Button btnDocentes;
+
+    @FXML
+    private Button btnCargosDocente;
+
+    @FXML
+    private Pane container_content;
+
+}
